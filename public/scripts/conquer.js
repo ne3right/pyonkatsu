89a87daf77919cf8f785fb044e938f7504430066
ne3right <ne3right@gmail.com>
2025-04-17 18:43:16 +0900

    #6 フロント部分改修

--- public/scripts/conquer.js
+++ public/scripts/conquer.js
@@ -56,31 +56,108 @@ function getProgressColor(value) {
   return `rgb(${r}, ${g}, ${b})`;
 }
 
-function replaceNullWithDefaultValues(data) {
-  for (let i = 1; i <= 5; i++) {
-    const groupKey = `group${i}`;
-    const noteKey = `group${i}_note`;
-    if (data[groupKey] === null) { data[groupKey] = 0;}
-    if (data[noteKey] === null) { data[noteKey] = "";}
+function getGeoJsonStyle(value) {
+  return {
+    color: 'black',
+    fillColor: getProgressColor(value),
+    fillOpacity: 0.7,
+    weight: 2,
   }
 }
 
-function setPolygonPopup(polygon, conquer, group) {
-  let popupContent = `<b>${conquer['subarea_name']}</b><br>`;
-  popupContent += `トータル: ${conquer['total_posting']}枚<br>`;
-  if (group != 'Total') {
-    popupContent += `${group}: ${conquer[group]}枚<br>備考:${conquer[`${group}_note`]}<br>`;
-  }
+function setPolygonPopup(polygon, areaname, value) {
+  let popupContent = `<b>${areaname}</b><br>`;
+  popupContent += `トータル: ${value}枚<br>`;
   polygon.bindPopup(popupContent);
+
+  // ▼ 一定値以上ならアイコンをマップ上に表示
+  if (value >= 100) {
+    const center = polygon.getBounds().getCenter();
+    let image = './usagi_100.png';
+    if (value >= 1000) {
+      image = './usagi_1000.png';
+    }
+    const icon = L.icon({
+      iconUrl: image,
+      iconSize: [57, 80],
+      iconAnchor: [28.5, 80],
+    });
+
+    L.marker([center.lat, center.lng], { icon: icon }).addTo(map);
+  }
 }
 
-function setMarkerWithTooltip(lat, lng, areaName, areaKey, areaId, totalPosting) { //全体マップの描画
-  const marker = L.marker([lat, lng]).addTo(map);
+function loadMapByArea(data, area_name = null, pref_id = null, city_id = null) {
+  for (let key in data) {
+    const item = data[key];
+
+    // パラメータによって処理を分岐
+    let geoJsonUrl = '';
+    let cpref = '';
+    let ccity = '';
+    let label = '';
+    let subarea = '';
+    let value = 0;
+    let is_detail = false
+
+    if (pref_id === null && city_id === null) {
+      // 全国表示
+      subarea = item.pref;
+      cpref = key;
+      label = item.pref;
+      value = item.sum;
+    } else if (pref_id !== null && city_id === null) {
+      // 都道府県表示
+      subarea = `${area_name}${item.city}`;
+      cpref = pref_id;
+      ccity = key;
+      label = item.city;
+      value = item.sum;
+    } else if (pref_id !== null && city_id !== null) {
+      // 市区町村詳細
+      subarea = `${area_name}${item.address}`;
+      label = item.address;
+      value = item.sum;
+      is_detail = true
+    }
+    geoJsonUrl = `https://uedayou.net/loa/${subarea}.geojson`;
+
+    fetch(geoJsonUrl)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`Failed to fetch geojson for ${label}`);
+        }
+        return response.json();
+      })
+      .then((geoData) => {
+        const polygon = L.geoJSON(geoData, { style: getGeoJsonStyle(value) });
+        polygon.addTo(map);
+
+        const centroid = polygon.getBounds().getCenter();
+        if(!is_detail) {
+          setMarkerWithTooltip(polygon, subarea, cpref, ccity, label, value);
+        } else {
+          setPolygonPopup(polygon, subarea, value);
+        }
+      })
+      .catch((error) => {
+        console.error('Error fetching geojson:', error);
+      });
+  }
+}
+
+function setMarkerWithTooltip(polygon, area_name, pref_id, city_id, label, value) { //全体マップの描画
+  let center = polygon.getBounds().getCenter();
+  if (customCenterOverrides[area_name]) {
+    center = customCenterOverrides[area_name];
+  }
+
+  const marker = L.marker([center.lat, center.lng]).addTo(map);
 
   const tooltipContent = `
   <div style="text-align: center;">
-    <strong>${areaName}</strong><br>
-    <span style="font-size: 12px; color: gray;"> ${totalPosting} 枚</span>
+    <strong>${label}</strong><br>
+    <span style="font-size: 12px; color: gray;"> ${value} 枚</span>
   </div>
 `;
 
@@ -93,110 +170,13 @@ function setMarkerWithTooltip(lat, lng, areaName, areaKey, areaId, totalPosting)
 
   marker.on('click', function () {
     const currentUrl = new URL(window.location.href);
-    currentUrl.searchParams.set('area_key', areaKey);
-    currentUrl.searchParams.set('area_id', areaId);
-    currentUrl.searchParams.set('lat', lat);
-    currentUrl.searchParams.set('lng', lng);
+    currentUrl.searchParams.set('area_name', area_name);
+    currentUrl.searchParams.set('pref_id', pref_id);
+    currentUrl.searchParams.set('city_id', city_id);
+    currentUrl.searchParams.set('lat', center.lat);
+    currentUrl.searchParams.set('lng', center.lng);
     window.location.href = currentUrl.toString();
   });
-}
-
-
-function fetchGeoJsonAndSetView(pref, zoomLevel) {
-  const geoPrefUrl = `https://uedayou.net/loa/${pref}.geojson`;
-  
-  return fetch(geoPrefUrl)
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error(`Failed to fetch geojson for ${pref}`);
-      }
-      return response.json();
-    })
-    .then((data) => {
-      const polygon = L.geoJSON(data);
-      const centroid = polygon.getBounds().getCenter();
-      map.setView([centroid.lat, centroid.lng], 11);
-    })
-    .catch((error) => {
-      console.error('Error fetching geojson:', error);
-    });
-}
-
-function getGroupGeoJsonStyle(value, group) { // 塗りつぶしマップのスタイルを決める
-  const styles = {
-    group1: { color: '#E63946', fillColor: '#E63946', fillOpacity: 0.1, weight: 1 }, // 赤
-    group2: { color: '#457B9D', fillColor: '#457B9D', fillOpacity: 0.1, weight: 1 }, // 青
-    group3: { color: '#2A9D8F', fillColor: '#2A9D8F', fillOpacity: 0.1, weight: 1 }, // 緑
-    group4: { color: '#F4A261', fillColor: '#F4A261', fillOpacity: 0.1, weight: 1 }, // オレンジ
-    group5: { color: '#9B5DE5', fillColor: '#9B5DE5', fillOpacity: 0.1, weight: 1 }, // 紫
-  };
-
-  return styles[group] || { 
-    color: 'black', 
-    fillColor: getProgressColor(value), 
-    fillOpacity: 0.4, 
-    weight: 2 
-  };
-}
-
-function fetchAndProcessGeoJson(dataSet, isDetailView) {
-  for (let [key, data] of Object.entries(dataSet)) {
-    const areaName = isDetailView ? data['subarea_name'] : data['area_name'];
-    const areaKey = isDetailView ? null : data['area_key']; // 詳細ビューでは areaKey は不要
-    const areaId = isDetailView ? null : data['area_id']; // 詳細ビューでは areaId は不要
-    const totalValue = isDetailView ? data['total_posting'] : conquerareatotal[data['area_id']];
-    
-    const geoJsonUrl = `https://uedayou.net/loa/${pref}${areaName}.geojson`;
-    if (isDetailView) {
-      replaceNullWithDefaultValues(data);
-    }
-
-    fetch(geoJsonUrl)
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error(`Failed to fetch geojson for ${areaName}`);
-        }
-        return response.json();
-      })
-      .then((geoData) => {
-
-        let groups = ['Total']; // 'Total' はデフォルトで追加
-        for (let i = 1; i <= 5; i++) {
-          let groupx = `group${i}`;
-          if (data[groupx] !== 0) { groups.push(groupx); }
-        }
-
-        if (isDetailView) {
-          //境界線はmapに書き込む
-          const polygon = L.geoJSON(geoData, { style: {color:'black', weight:1,fillOpacity:0,}});
-          polygon.bindPopup(data['subarea_name']);/*`<b>${conquer['subarea_name']}</b><br>`;*/
-          polygon.addTo(map);
-          groups.forEach(group => {
-            const polygon = L.geoJSON(geoData, { style: getGroupGeoJsonStyle(totalValue, group) });
-            setPolygonPopup(polygon, data, group);
-            polygon.addTo(overlays[group]);
-          });
-        } else {
-          const polygon = L.geoJSON(geoData, { style: getGroupGeoJsonStyle(totalValue, 'Total') });
-          polygon.addTo(map);
-          const centroid = polygon.getBounds().getCenter();
-          setMarkerWithTooltip(centroid.lat, centroid.lng, areaName, areaKey, areaId, totalValue);
-        }
-
-      })
-      .catch((error) => {
-        console.error('Error fetching geojson:', error);
-      });
-  }
-}
-
-function getGeoJsonStyle(value) {
-  return {
-    color: 'black',
-    fillColor: getProgressColor(value),
-    fillOpacity: 0.7,
-    weight: 2,
-  }
 }
 
 var map = L.map("map", { preferCanvas: true, zoomControl: false }).setView([35.669400214188606, 139.48343915372877], 11);
@@ -207,48 +187,38 @@ const baseLayers = {
   '国土地理院地図': japanBaseMap,
 };
 
-const overlays = {
-  'group1':  L.layerGroup(),
-  'group2':  L.layerGroup(),
-  'group3':  L.layerGroup(),
-  'group4':  L.layerGroup(),
-  'group5':  L.layerGroup(),
-  'Total':  L.layerGroup(),
-};
-
 japanBaseMap.addTo(map);
-map.addLayer(overlays['Total']);
-let layerControl = L.control.layers(baseLayers, overlays, { position: "topleft" }).addTo(map);
+let layerControl = L.control.layers(baseLayers, null, { position: "topleft" }).addTo(map);
 
 let areaList;
 let progress;
-const area_key = getParamFromUrl("area_key");
-const area_id = getParamFromUrl("area_id");
-const pref = getParamFromUrl("pref");
+
+const area_name = getParamFromUrl("area_name");
+const pref_id = getParamFromUrl("pref_id");
+const city_id = getParamFromUrl("city_id");
 const lat = getParamFromUrl("lat");
 const lng = getParamFromUrl("lng");
 
-Promise.all([getConquerblock(), getConquerdata(area_key), getConquerareatotal()]).then(function (res) {
-  conquerblock = res[0];
-  conquerdata = res[1];
-  conquerareatotal = res[2];
+Promise.all([ getPostingData(pref_id, city_id)]).then(function (res) {
+  postingdata = res[0];
 
-  let areaTotalValue;
-  if (area_key === null) {
-    // area_keyが定義されていない場合、全体マップ
-    map.removeControl(layerControl); // 既存のレイヤーコントロールを削除
-    layerControl = L.control.layers(baseLayers, {}, { position: "topleft" }).addTo(map); // overlaysなしで再作成
-    fetchGeoJsonAndSetView(pref, 11); // 都道府県の中心地を取得して移動
-    areaTotalValue = conquerareatotal['total']; // 全体データ
-    fetchAndProcessGeoJson(conquerblock, false);
-  } else {
-    // area_keyが定義されている場合、詳細マップ(ポスター枚数による塗分け)を表示する
+  const total = Object.values(postingdata).reduce((acc, item) => acc + (item.sum || 0), 0);
+
+  if (pref_id === null) {
+    // 全国
+    loadMapByArea(postingdata);
+  } else if (pref_id !== null && (city_id === null || city_id === "")) {
+    // 都道府県マップ
+    map.setView([lat, lng], 11);
+    loadMapByArea(postingdata,area_name, pref_id, null);
+  } else if (pref_id !== null && city_id !== null) {
+    // 市区町村マップ
     map.setView([lat, lng], 14);
-    areaTotalValue = conquerareatotal[area_id]; // 特定エリアのデータ
-    fetchAndProcessGeoJson(conquerdata, true);
+    loadMapByArea(postingdata, area_name, pref_id, city_id);
   }
+
   //マップ合計と凡例を表示
-  areatotalBox(areaTotalValue, 'topright').addTo(map)
+  areatotalBox(total, 'topright').addTo(map)
   legend().addTo(map);
 
 }).catch((error) => {
